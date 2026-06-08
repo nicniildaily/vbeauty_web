aad1fda99f9d256b7bd141711a5566934c129ce7
nicniildaily <nicniildaily@users.noreply.github.com>
2026-06-08 08:25:23 +0000

    deploy: 16cd64a5f39d28650532e86fdea7d3e2e6a146fe

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4142718533" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1679964782" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
